edba15ec96115fd41730251818fd54c893d8641c
justinmihigo <mihigojustin28@gmail.com>
2024-10-28 22:40:42 +0200

    ch: reconfigure multer by removing destinations of uploads

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,10 +4,10 @@ import multer from "multer"
 const storage= multer.diskStorage({
     filename: function(req, file, cb){
         cb(null, file.originalname)
-    },
-    destination:function(req, file, cb){
-        cb(null, './public/')
     }
+    // destination:function(req, file, cb){
+    //     cb(null, './public/')
+    // }
 });
 
 const fileFilter= (req:Request,file:any,cb:any)=>{
